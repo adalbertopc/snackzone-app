b91bb0e13dea005fd36742548ee8398ee2081de3
adalbertopc <adalbertinho90@gmail.com>
2021-04-30 01:21:40 -0700

    feat: add cartproducts context/reducer to  show in cartScreen (primitive version)

--- screens/Cart.js
+++ screens/Cart.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 
 import { COLORS, FONTS, icons, SIZES, GOOGLE_API_KEY } from '../constants';
+import { CartContext } from '../src/contexts/CartContext';
 
 export const Cart = () => {
+	const { cartProducts, dispatch } = useContext(CartContext);
+	useEffect(() => {
+		console.log(cartProducts);
+	}, [cartProducts]);
 	return (
-		<View>
+		<View style={{ flex: 1 }}>
 			<Text style={{ ...FONTS.h1 }}>Carrito Screen</Text>
+			<View style={{ flex: 1, flexDirection: 'column' }}>
+				{cartProducts.map((product) => (
+					<View
+						style={{
+							flexDirection: 'row',
+							justifyContent: 'space-between',
+						}}
+					>
+						<Text style={{ fontSize: 20 }}>Producto: {product.menuId}</Text>
+						<Text>Cantidad:{product.qty}</Text>
+					</View>
+				))}
+			</View>
 		</View>
 	);
 };
